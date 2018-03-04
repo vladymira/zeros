09f2988c78e3cb47a819b5e2da64043661b866e7
Alex <alex.fallen.part@gmail.com>
2018-03-04 20:37:47 +0300

    fuction added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount(num) {
+    var czero = 0;
+
+
+    for (var i = 5; Math.floor(num / i) >= 1; i = i * 5) {
+        czero = czero + Math.floor(num / i);
+
+    }
+    
+    return czero;
 }
